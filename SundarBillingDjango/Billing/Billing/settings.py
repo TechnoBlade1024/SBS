acf554609a54e78e2aea5f1a474b349c7082d7a9
technoblade1024 <technoblade1021@gmail.com>
2021-09-30 23:56:06 +0530

    Initial Django Commit

--- SundarBillingDjango/Billing/Billing/settings.py
+++ SundarBillingDjango/Billing/Billing/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'Billing.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+       'ENGINE': 'django.db.backends.mysql',
+       'NAME': "sundar_billing",
+       'USER': "root",
+       'PASSWORD': "PassWord@123 ",
+       'HOST': "127.0.0.1",
+       'PORT': "3306",
+   }
 }
 
 
